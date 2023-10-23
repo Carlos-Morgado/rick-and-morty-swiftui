990d4cdaa9ddf64a268cfd13f34b7d17f4224525
Carlos Morgado <cmorgadoalarcon@gmail.com>
2023-10-23 11:58:25 +0200

    Initializing Rick&Morty SwiftUI project

---
+++ rick-and-morty-swift-ui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rick-and-morty-swift-ui
+//
+//  Created by Carlos Morgado on 23/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rick-and-morty-swift-ui/rick_and_morty_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  rick_and_morty_swift_uiApp.swift
+//  rick-and-morty-swift-ui
+//
+//  Created by Carlos Morgado on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct rick_and_morty_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
